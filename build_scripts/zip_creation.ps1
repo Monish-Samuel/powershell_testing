131b56d7850e123a7d3e04313e402c6cd27c6c18
Monish-Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2021-09-11 15:20:32 +0530

    Update zip_creation.ps1

--- build_scripts/zip_creation.ps1
+++ build_scripts/zip_creation.ps1
@@ -1,9 +1,9 @@
-New-Item -Path '..\temp' -ItemType Directory
-New-Item -Path '..\temp\src' -ItemType Directory
-Copy-Item -Path ..\src\* -Destination ..\temp\src
+New-Item -Path '.\temp' -ItemType Directory
+New-Item -Path '.\temp\src' -ItemType Directory
+Copy-Item -Path .\src\* -Destination .\temp\src
 $buildNo= $env:buildNo
-Compress-Archive -Path ..\temp\src -DestinationPath ..\temp\app-$buildNo.zip
+Compress-Archive -Path .\temp\src -DestinationPath .\temp\app-$buildNo.zip
 Sleep -s 5
 Write-Host "Zip File is Created"
-$Value= Get-ChildItem -Path ..\temp\*.zip -Name
+$Value= Get-ChildItem -Path .\temp\*.zip -Name
 Write-Host "ZIP FILE NAME: $Value"
