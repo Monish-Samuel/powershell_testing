f622dfa807b23513d4b350c15749cced3be54b07
Monish-Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2021-09-11 16:00:47 +0530

    Update zip_creation.ps1

--- build_scripts/zip_creation.ps1
+++ build_scripts/zip_creation.ps1
@@ -1,17 +1,17 @@
-if(Test-Path -Path .\temp -PathType Any){
-	Write-Host "Deleting .\temp"
-	Get-ChildItem -Path ".\" -Directory -Filter "temp" | Remove-Item -Recurse -Confirm:$false -Force
+if(Test-Path -Path .\source-repo -PathType Any){
+	Write-Host "Deleting .\source-repo"
+	Get-ChildItem -Path ".\" -Directory -Filter "source-repo" | Remove-Item -Recurse -Confirm:$false -Force
 	Sleep -s 5
 }
-if(-not(Test-Path -Path .\temp\*.zip -PathType Leaf)){
+if(-not(Test-Path -Path .\source-repo\*.zip -PathType Leaf)){
 	Write-Host "Creating New path for ZIP build"
-	New-Item -Path '.\temp' -ItemType Directory
-	New-Item -Path '.\temp\src' -ItemType Directory
-	Copy-Item -Path .\src\* -Destination .\temp\src
+	New-Item -Path '.\source-repo' -ItemType Directory
+	New-Item -Path '.\source-repo\src' -ItemType Directory
+	Copy-Item -Path .\src\* -Destination .\source-repo\src
 	$buildNo= $env:buildNo
-	Compress-Archive -Path .\temp\src -DestinationPath .\temp\app-$buildNo.zip
+	Compress-Archive -Path .\source-repo\src -DestinationPath .\source-repo\app-$buildNo.zip
 	Sleep -s 5
 	Write-Host "Zip File is Created"
-	$Value= Get-ChildItem -Path .\temp\*.zip -Name
+	$Value= Get-ChildItem -Path .\source-repo\*.zip -Name
 	Write-Host "ZIP FILE NAME: $Value"
 }
