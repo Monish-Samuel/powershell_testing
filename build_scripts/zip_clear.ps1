f97194f2b86caf0cecdcd6218c7640f326f2cbb3
Monish-Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2021-09-11 20:19:56 +0530

    Update and rename zip_clear.ps1 to artifact_download.ps1

--- build_scripts/zip_clear.ps1
+++
@@ -1,6 +0,0 @@
-$value=$env:buildNo
-if(Test-Path -Path .\*-'$value'.zip -PathType Any){
-	Get-ChildItem -Path ".\source-repo" | Remove-Item -Recurse -Confirm:$false -Force
-	Move-Item -Path .\*-'$value'.zip -Destination .\source-repo
-	Get-ChildItem -Path '.\' *.zip | foreach { Remove-Item -Path $_.FullName }
-}
