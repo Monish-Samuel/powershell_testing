d98adaf89f0ecf858d4f0292642df76da2b7c696
Monish-Samuel <samuelmonish1@gmail.com>
2021-09-11 15:30:54 +0530

    zip update

--- build_scripts/zip_creation.ps1
+++ build_scripts/zip_creation.ps1
@@ -1,9 +1,18 @@
-New-Item -Path '.\temp' -ItemType Directory
-New-Item -Path '.\temp\src' -ItemType Directory
-Copy-Item -Path .\src\* -Destination .\temp\src
-$buildNo= $env:buildNo
-Compress-Archive -Path .\temp\src -DestinationPath .\temp\app-$buildNo.zip
-Sleep -s 5
-Write-Host "Zip File is Created"
-$Value= Get-ChildItem -Path .\temp\*.zip -Name
-Write-Host "ZIP FILE NAME: $Value"
+$file= '.\temp'
+if(Test-Path -Path $file -PathType Leaf){
+	Write-Host "Deleting [$file]"
+	Remove-Item -Path $file -Force
+	Sleep -s 5
+}
+else{
+	Write-Host "Creating New path for ZIP build"
+	New-Item -Path '$file' -ItemType Directory
+	New-Item -Path '$file\src' -ItemType Directory
+	Copy-Item -Path .\src\* -Destination $file\src
+	$buildNo= $env:buildNo
+	Compress-Archive -Path $file\src -DestinationPath $file\app-$buildNo.zip
+	Sleep -s 5
+	Write-Host "Zip File is Created"
+	$Value= Get-ChildItem -Path $file\*.zip -Name
+	Write-Host "ZIP FILE NAME: $Value"
+}
