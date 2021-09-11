42d4141a737497d90b9508bccccec381c0284ce4
Monish-Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2021-09-11 21:29:17 +0530

    Update build_number.ps1

--- build_scripts/build_number.ps1
+++ build_scripts/build_number.ps1
@@ -5,5 +5,5 @@ $buildnumber= $env:BUILD_NUMBER
 $buildNo= $props.'MAJOR_VERSION'+"."+$props.'MINOR_VERSION'+"."+$props.'PATCH_VERSION'+"."+$buildnumber
 Write-Output $buildNo | Out-File "buildNo.txt"
 
-//Clean up Artifacts Before New build and Publish
+#Clean up Artifacts Before New build and Publish
 Get-ChildItem -Path ".\" -Directory -Filter "artifacts" | Remove-Item -Recurse -Confirm:$false -Force
