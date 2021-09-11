6665d01d894078603027b64330edfb42ef73ab06
Monish-Samuel <samuelmonish1@gmail.com>
2021-09-11 14:50:27 +0530

    folder path

--- build_number.ps1
+++
@@ -1,6 +0,0 @@
-$props= Get-Content 'build.properties'
-$props= $props -join [Environment]::Newline
-$props= ConvertFrom-StringData($props)
-$buildnumber= $env:BUILD_NUMBER
-$buildNo= $props.'MAJOR_VERSION'+"."+$props.'MINOR_VERSION'+"."+$props.'PATCH_VERSION'+"."+$buildnumber
-Write-Output $buildNo | Out-File "buildNo.txt"
