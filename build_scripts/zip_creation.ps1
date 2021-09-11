92890cd221309e48be93e9831da14b0ebda11f3d
Monish-Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2021-09-11 15:51:56 +0530

    Update zip_creation.ps1

--- build_scripts/zip_creation.ps1
+++ build_scripts/zip_creation.ps1
@@ -1,5 +1,9 @@
-if(-not(Test-Path -Path .\temp\*.zip -PathType Leaf)){
-try{
+if(Test-Path -Path .\temp\*.zip -PathType Leaf){
+	Write-Host "Deleting .\temp"
+	Get-ChildItem -Path ".\temp\" | Remove-Item -Recurse -Confirm:$false -Force
+	Sleep -s 5
+}
+if(Test-Path -Path .\temp\*.zip -PathType Leaf){
 	Write-Host "Creating New path for ZIP build"
 	New-Item -Path '.\temp' -ItemType Directory
 	New-Item -Path '.\temp\src' -ItemType Directory
@@ -10,12 +14,4 @@ try{
 	Write-Host "Zip File is Created"
 	$Value= Get-ChildItem -Path .\temp\*.zip -Name
 	Write-Host "ZIP FILE NAME: $Value"
-	}catch {
-         throw $_.Exception.Message
-     }
-}
-else{
-	Write-Host "Deleting .\temp"
-	Remove-Item -Path .\temp -Force -Confirm:$false
-	Sleep -s 5
 }
