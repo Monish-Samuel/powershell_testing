6ad5aab45d0557e1af0c328ebaa0addb8e71ae26
Monish-Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2021-09-11 13:28:08 +0530

    Update build_number.ps1

--- build_number.ps1
+++ build_number.ps1
@@ -2,4 +2,5 @@ $props= Get-Content 'build.properties'
 $props= $props -join [Environment]::Newline
 $props= ConvertFrom-StringData($props)
 $buildNo= $props.'MAJOR_VERSION'+"."+$props.'MINOR_VERSION'+"."+$props.'PATCH_VERSION'
+Write-Output $buildNo
 Write-Host $buildNo 
