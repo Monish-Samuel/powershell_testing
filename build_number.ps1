3fc83be8a4312346650b4b9a4edfedcd738d1647
Monish-Samuel <samuelmonish1@gmail.com>
2021-09-11 13:20:55 +0530

    check

---
+++ build_number.ps1
@@ -0,0 +1,5 @@
+$props= Get-Content 'build.properties'
+$props= $props -join [Environment]::Newline
+$props= ConvertFrom-StringData($props)
+$buildNo= $props.'MAJOR_VERSION'+"."+$props.'MINOR_VERSION'+"."+$props.'PATCH_VERSION'
+Write-Host $buildNo 
